03e7e4935d8a29c1003fdd2071bd65d91833d668
bekimsylqazs <bekimsylqa@gmail.com>
2020-02-12 15:56:25 +0100

    first commit

---
+++ Project/app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Tag;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function show(Article $article)
+    {
+        return view('articles.show',['article'=>$article]);
+    }
+
+    public function index()
+    {
+        if(request('tag'))
+        {
+            $articles = Tag::where('name',request('tag'))->firstOrFail()->articles;
+        }
+        else{
+            $articles = Article::latest()->get();
+        }
+
+        return view('articles.index',['articles'=>$articles]);
+    }
+
+    public function create()
+    {
+        return view('articles.create',[
+            'tags'=> Tag::all()
+        ]);
+    }
+
+    public function store()
+    {
+
+        //Article::create($this->validateArticle());
+
+        $this->validateArticle();
+
+        $article = new Article(request(['title','excerpt','body']));
+        $article->user_id = 1;
+        $article->save();
+
+        $article->tags()->attach(request('tags'));
+        return redirect(route('articles.index'));
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit',compact('article'));
+    }
+
+    public function update(Article $article)
+    {
+
+        $article->update($this->validateArticle());
+
+        return redirect($article->path());
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    protected function validateArticle(){
+
+        return request()->validate([
+            'title'=>'required',
+            'excerpt'=>'required',
+            'body'=>'required',
+            'tags'=>'exists:tags,id'
+        ]);
+    }
+}
---
+++ Project/resources/views/articles/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layout')
+
+@section('header')
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
+
+@endsection
+
+@section('content')
+
+    <div id="wrapper">
+        <div id="page" class="container">
+            <H1 class="heading has-text-weight-bold is-size-4">Update Article</H1>
+
+            <form method="POST" action="/articles/{{ $article->id }}">
+                @csrf
+                @method('PUT')
+
+                <div class="field">
+                    <label class="label" for="title">Title</label>
+                    <div class="control">
+                        <input class="input" type="text" name="title" id="title" value="{{ $article->title }}">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="excerpt">Excerpt</label>
+                    <div class="control">
+                        <textarea class="textarea"  name="excerpt" id="excerpt">{{ $article->excerpt }}</textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="body">Body</label>
+                    <div class="control">
+                        <textarea class="textarea"  name="body" id="body">{{ $article->body }}</textarea>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div clas="control">
+                        <button class="button is-link" type="submit">Submit</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+@endsection
